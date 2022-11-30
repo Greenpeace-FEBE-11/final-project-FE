97bf624508d12e6e7235cd5f770b899f20a3297a
USERNAME <firdauskun00@gmail.com>
2022-11-30 21:47:52 +0800

    test

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,11 +5,19 @@ import Dampak from '../Components/HomePage/Dampak'
 import FetchDampakSinopsis from '../Components/HomePage/FetchDampakSinopsis'
 import HomePageBG from '../Components/HomePage/HomePageBG'
 import PenanggulanganSinopsis from '../Components/HomePage/PenanggulanganSinopsis'
+import Login from '../Components/Logres/Login'
 import Navbar from '../Components/Navbar'
 
 
-function HomePage() {
+const HomePage =()=> {
+  const cekEmail = localStorage.getItem("account");
+  const cekPassword = localStorage.getItem("pass");
+
   return (
+    <>
+    {!cekEmail && !cekPassword? (
+      <Login/>
+    ) : (
     <div id="HomePagee">
       <Navbar/>
         <HomePageBG/>
@@ -18,9 +26,11 @@ function HomePage() {
         <ContactUs/>
         {/* <Dampak/> */}
         <PenanggulanganSinopsis/>
-
     </div>    
+    )}
+    </>
   )
+  
 }
 
 export default HomePage
--- src/Pages/PenanggulanganPage.jsx
+++ src/Pages/PenanggulanganPage.jsx
@@ -1,27 +1,36 @@
-import React from "react"
-import Penanggulangan1 from "../Components/Penanggulangan/Penanggulangan1"
-import Penanggulangan2 from "../Components/Penanggulangan/Penanggulangan2"
-import DataFetching1 from "../Components/DataFetching/DataFetching1"
-import DataFetching2 from "../Components/DataFetching/DataFetching2"
-import DataFetching3 from "../Components/DataFetching/DataFetching3"
-import Penanggulangan3 from "../Components/Penanggulangan/Penanggulangan3"
-import Navbar from "../Components/Navbar"
+import React from "react";
+import Penanggulangan1 from "../Components/Penanggulangan/Penanggulangan1";
+import Penanggulangan2 from "../Components/Penanggulangan/Penanggulangan2";
+import DataFetching1 from "../Components/DataFetching/DataFetching1";
+import DataFetching2 from "../Components/DataFetching/DataFetching2";
+import DataFetching3 from "../Components/DataFetching/DataFetching3";
+import Penanggulangan3 from "../Components/Penanggulangan/Penanggulangan3";
+import Navbar from "../Components/Navbar";
+import Login from "../Components/Logres/Login";
 
 // import '../css/readmore.css'
 // import '../css/style-penanggulangan.css'
 
 const PenanggulanganPage = () => {
-    return(
+  const cekEmail = localStorage.getItem("account");
+  const cekPassword = localStorage.getItem("pass");
+  return (
+    <>
+      {!cekEmail && !cekPassword ? (
+        <Login />
+      ) : (
         <div id="penanggulanganpagee">
-            <Navbar/>
-            <DataFetching1/>
-            <Penanggulangan1/>
-            <DataFetching2/>
-            <Penanggulangan2/>
-            <DataFetching3/>
-            <Penanggulangan3/>
+          <Navbar />
+          <DataFetching1 />
+          <Penanggulangan1 />
+          <DataFetching2 />
+          <Penanggulangan2 />
+          <DataFetching3 />
+          <Penanggulangan3 />
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default PenanggulanganPage
+export default PenanggulanganPage;
